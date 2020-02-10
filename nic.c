f9e5b4c52f09c8fdb51ed7d0d4640bb4e56a19ca
leojojo <setsunal.fujita@gmail.com>
2020-02-11 06:28:37 +0900

    mac address

--- nic.c
+++ nic.c
@@ -23,20 +23,16 @@ set_nic_reg(uint32_t reg, uint32_t val)
 void
 disable_nic_interrupt(struct pci_func *pci)
 {
-  /* 一旦、コマンドとステータスを読み出す */
   uint32_t conf_data = pci_conf_read(*pci, PCI_CONF_STATUS_COMMAND);
-  /* ステータス(上位16ビット)をクリア */
   conf_data &= 0x0000ffff;
-  /* コマンドに割り込み無効設定 */
   conf_data |= PCI_COM_INTR_DIS;
-  /* コマンドとステータスに書き戻す */
   pci_conf_write(*pci, PCI_CONF_STATUS_COMMAND, conf_data);
-  /* NICの割り込みをIMC(Interrupt Mask Clear Register)で全て無効化 */
   set_nic_reg(NIC_REG_IMC, 0xffffffff);
 }
 
 void
-dump_nic_ims() {
+dump_nic_ims()
+{
   uint32_t ims = get_nic_reg(NIC_REG_IMS);
   cprintf("%x\n",ims);
   set_nic_reg(NIC_REG_IMS,0x0000beef);
@@ -44,12 +40,50 @@ dump_nic_ims() {
   cprintf("%x\n",ims);
 }
 
+uint32_t
+get_eeprom(uint16_t eeprom_addr)
+{
+  set_nic_reg(NIC_REG_EERD, (eeprom_addr << NIC_EERD_ADDRESS_SHIFT) | NIC_EERD_START);
+  uint32_t eerd = get_nic_reg(NIC_REG_EERD);
+  while (1) {
+    if (eerd & NIC_EERD_DONE) {
+      return eerd >> NIC_EERD_DATA_SHIFT;
+    }
+  }
+}
+
+uint32_t*
+get_mac_addr(uint32_t* nic_mac_addr)
+{
+  uint32_t mac_1_0 = get_eeprom(0x00);
+  uint32_t mac_3_2 = get_eeprom(0x01);
+  uint32_t mac_5_4 = get_eeprom(0x02);
+
+  nic_mac_addr[0] = mac_1_0 & 0x00ff;
+  nic_mac_addr[1] = mac_1_0 >> 8;
+  nic_mac_addr[2] = mac_3_2 & 0x00ff;
+  nic_mac_addr[3] = mac_3_2 >> 8;
+  nic_mac_addr[4] = mac_5_4 & 0x00ff;
+  nic_mac_addr[5] = mac_5_4 >> 8;
+
+  return nic_mac_addr;
+}
+
+void
+print_mac_addr(uint32_t *nic_mac_addr)
+{
+  for(int i = 0; i < 6; i++) {
+    cprintf("%d:",nic_mac_addr[i]);
+  }
+  cprintf("\n");
+}
+
 void
 nicinit(struct pci_func *pci)
 {
-  /* NICのレジスタのベースアドレスを取得しておく */
+  uint32_t nic_mac_addr[6];
   nic_reg_base = get_base_address(pci);
-  /* NICの割り込みを全て無効にする */
   disable_nic_interrupt(pci);
-  dump_nic_ims();
+  print_mac_addr(get_mac_addr(nic_mac_addr));
+  //dump_nic_ims();
 }
--- nic.h
+++ nic.h
@@ -6,8 +6,15 @@
 
 #define NIC_REG_IMS     0x00d0      // irq mask set
 #define NIC_REG_IMC    0x00d8       // irq mask clear
+#define NIC_REG_EERD    0x0014       // EEPROM Read Register
+
+#define NIC_EERD_START  (1U << 0)
+#define NIC_EERD_DONE (1U << 4)
+#define NIC_EERD_ADDRESS_SHIFT  8
+#define NIC_EERD_DATA_SHIFT 16
 
 void nicinit(struct pci_func *pci);
 void dump_nic_ims();
+uint32_t* get_mac_addr();
 
 #endif
